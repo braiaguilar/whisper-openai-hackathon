878dc0f93b2152d94060f03d3f704384387c0617
AguilarBrian <brianisaiasaguilar@gmail.com>
2022-10-16 11:45:08 -0300

    Proper use of Whisper

--- index.py
+++ index.py
@@ -1,18 +1,30 @@
 import os
 import whisper
 import openai
+import numpy as np
+import torch
 from dotenv import load_dotenv
 
 load_dotenv()
-
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-model = whisper.load_model("base")
+torch.cuda.is_available()
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+model = whisper.load_model("base", device=DEVICE)
+print(
+    "### Model info: "
+    f"Model is {'multilingual' if model.is_multilingual else 'English-only'} "
+    f"and has {sum(np.prod(p.shape) for p in model.parameters()):,} parameters."
+)
 
 # here we would use the audio file from the user
 whisperOutput = model.transcribe("audio.wav")
 
-prompt = whisperOutput["text"]
+# here we should add the order to summarize the text
+prompt = "Write a short summary of this text:" + whisperOutput["text"]
+
+print("### Prompt for GPT-3: " + prompt)
 
 response = openai.Completion.create(
     model="text-davinci-002",
@@ -23,3 +35,5 @@ response = openai.Completion.create(
     frequency_penalty=0,
     presence_penalty=0,
 )
+
+print("### GPT-3 output: " + response)
